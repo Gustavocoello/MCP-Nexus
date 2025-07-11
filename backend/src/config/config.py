748888d51f040f77a31ab5893b2a948a90dfb385
Gustavocoello <coellog634@gmail.com>
2025-07-11 11:24:23 -0500

    Add new config to flask

--- backend/src/config/config.py
+++ backend/src/config/config.py
@@ -5,13 +5,20 @@ load_dotenv()
 
 class Config:
     """Configuración general de la aplicación."""
-    #SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('USER_BD')}:{os.getenv('PASS_BD')}@{os.getenv('ROOT_BD')}/{os.getenv('NAME_BD')}"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Sesiones Redis
     SESSION_TYPE = "redis"
     SESSION_COOKIE_NAME = "session"
-    SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = True  # ✅ debe estar en True para cross-origin HTTPS
+    SESSION_COOKIE_SAMESITE = "None"  # ✅ permite enviar cookies desde otro dominio
+
+    # También para recordar sesiones con LoginManager (opcional pero recomendado)
+    REMEMBER_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SAMESITE = "None"
+
+    # Redis y Secret
     REDIS_URL = os.getenv("REDIS_URL")
     SECRET_KEY = os.getenv("SECRET_KEY")
-
