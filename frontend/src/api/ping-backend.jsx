99fa20ca7636f322a49b717ad444adff398a0047
Gustavocoello <coellog634@gmail.com>
2025-09-16 21:40:53 -0500

    Add vercel function update + new folder in frontend3

--- frontend/src/api/ping-backend.jsx
+++
@@ -1,6 +0,0 @@
-import { pingBackend } from '../service/ping_backend';
-
-export default async function handler(req, res) {
-  const result = await pingBackend();
-  res.status(200).json(result);
-}
